b134812bf520f55d57f424d683ff1bae896d265e
Luka Bradić <bradicluka04@gmail.com>
2026-05-14 13:37:14 +0200

    [ADD] - backend base setup

---
+++ backend/service/src/test/java/com/ftn/sbnz/f1/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.sbnz.f1.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
