1d6d6c31166181503bed88e46a781bda7be111fa
Luka Bradić <bradicluka04@gmail.com>
2026-05-14 18:23:30 +0200

    [MOD] - mod project mvn structure

---
+++ backend/service/src/main/java/com/ftn/sbnz/f1/service/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.ftn.sbnz.f1.service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        .allowedOriginPatterns("*")
+        .allowedMethods("*")
+        .allowedHeaders("*");
+    }
+}
